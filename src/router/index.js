6b47e4fac05dd0476f3cf2d86b90d78ea1fae9d6
Thiện Phạm <phamquangthien99@gmai.com>
2023-03-18 10:42:23 +0700

    add project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
 const routes = [
   {
     path: '/',
@@ -14,9 +19,38 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/:category_slug/:product_slug',
+    name: 'product',
+    component: Product
+  }, 
+  {
+    path: '/:category_slug',
+    name: 'Category',
+    component: Category
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: Search
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+  },  
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },  {
+    path: '/log-in',
+    name: 'Login',
+    component: Login
   }
-]
 
+]
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="page-login">
+        <h1>Login</h1>
+        <form @submit.prevent="submitForm">
+            <div class="field">
+                <label>UserName</label>
+                <div class="control">
+                    <input type="text" class="input" v-model="username">
+                </div>
+            </div>
+            <div class="field">
+                <label>Password</label>
+                <div class="control">
+                    <input type="password" class="input" v-model="password">
+                </div>
+            </div>        
+            <div class="notification is-danger" v-if="errors.length">
+                <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <button class="button is-dark"> SignUp </button>
+                </div>
+            </div>
+            <hr>
+            Or <router-link to="/log-in" >Click here</router-link> to login
+        </form>
+    </div>
+</template>
+<script>
+export default{
+    name: 'Login',
+    data(){
+        return
+    },
+}
+</script>
